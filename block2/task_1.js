4cada6083512ebebfc8b651cb20a4a7bcf6fdb90
s1dexdev <maksym.se@gmail.com>
2021-10-18 12:40:17 +0300

    update

--- block2/task_1.js
+++ block2/task_1.js
@@ -21,3 +21,25 @@ Function.prototype.customCall = function (context, ...args) {
         }
     })();
 };
+
+Function.prototype.customBind = function (context, ...args) {
+    const func = this;
+
+    return function (...args2) {
+        context = context || this;
+
+        if (typeof context === 'object') {
+            let method = Symbol();
+
+            context[method] = func;
+            context[method](...args, ...args2);
+
+            delete context[method];
+        } else {
+            const result = Object(context);
+
+            result.method = func;
+            result.method(...args, ...args2);
+        }
+    };
+};
