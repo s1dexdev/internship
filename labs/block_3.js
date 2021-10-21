1036f81a42a77cb9ff752319cc3aeaf38adf3b7b
s1dexdev <maksym.se@gmail.com>
2021-10-21 18:29:56 +0300

    added block_3

--- labs/block_3.js
+++ labs/block_3.js
@@ -1,16 +1,118 @@
 // Task 1 -----------
 
+// class Node {
+//     #value;
+//     #right;
+//     #left;
+//     #parent;
+
+//     constructor() {
+//         this.#value = null;
+//         this.#right = null;
+//         this.#left = null;
+//     }
+
+//     insert(value, node) {
+//         node = node || this;
+
+//         if (node.#value === null) {
+//             node.#value = value;
+
+//             return true;
+//         }
+
+//         if (node.#value > value) {
+//             if (node.#left === null) {
+//                 node.#left = new Node();
+//                 node.#left.#parent = node;
+//             }
+
+//             return this.insert(value, node.#left);
+//         }
+
+//         if (node.#value < value) {
+//             if (node.#right === null) {
+//                 node.#right = new Node();
+//                 node.#right.#parent = node;
+//             }
+
+//             return this.insert(value, node.#right);
+//         }
+//     }
+
+//     findByValue(value, node) {
+//         node = node || this;
+
+//         if (node.#value === value) {
+//             return node;
+//         }
+
+//         if (node.#left === null && node.#right === null) {
+//             return false;
+//         }
+
+//         if (node.#value > value) {
+//             return this.findByValue(value, node.#left);
+//         }
+
+//         if (node.#value < value) {
+//             return this.findByValue(value, node.#right);
+//         }
+//     }
+
+//     remove(value) {
+//         let removeElem = this.findByValue(value);
+
+//         if (!removeElem) {
+//             return false;
+//         }
+
+//         // Удаление узла без потомков
+//         if (removeElem.#right === null && removeElem.#left === null) {
+//             if (removeElem.#value > removeElem.#parent.#value) {
+//                 removeElem.#parent.#right = null;
+
+//                 return true;
+//             }
+
+//             removeElem.#parent.#left = null;
+
+//             return true;
+//         }
+
+//         // Удаление узла с двумя потомками
+//         if (removeElem.#right && removeElem.#left) {
+//             return true;
+//         }
+
+//         // Удаление узла с одним потомком
+//         if (removeElem.#right === null || removeElem.#left === null) {
+//             const child = removeElem.#right ?? removeElem.#left;
+
+//             child.#parent = removeElem.#parent;
+
+//             if (removeElem.#value < removeElem.#parent.#value) {
+//                 removeElem.#parent.#left = child;
+
+//                 return true;
+//             }
+
+//             removeElem.#parent.#right = child;
+
+//             return true;
+//         }
+//     }
+// }
+
 class Node {
     #value;
     #right;
     #left;
-    #parent;
 
     constructor() {
         this.#value = null;
         this.#right = null;
         this.#left = null;
-        this.#parent = null;
     }
 
     insert(value, node) {
@@ -25,7 +127,6 @@ class Node {
         if (node.#value > value) {
             if (node.#left === null) {
                 node.#left = new Node();
-                node.#left.#parent = node;
             }
 
             return this.insert(value, node.#left);
@@ -34,7 +135,6 @@ class Node {
         if (node.#value < value) {
             if (node.#right === null) {
                 node.#right = new Node();
-                node.#right.#parent = node;
             }
 
             return this.insert(value, node.#right);
@@ -48,69 +148,92 @@ class Node {
             return node;
         }
 
-        if (node.#left === null && node.#right === null) {
-            return false;
-        }
-
         if (node.#value > value) {
+            if (node.#left === null) {
+                return false;
+            }
             return this.findByValue(value, node.#left);
         }
 
         if (node.#value < value) {
+            if (node.#right === null) {
+                return false;
+            }
+
             return this.findByValue(value, node.#right);
         }
     }
 
-    remove(value) {
+    remove(value, node) {
+        node = node || this;
         let removeElem = this.findByValue(value);
 
-        if (!removeElem) {
+        if (removeElem === null) {
             return false;
         }
 
-        // Удаление узла без потомков
-        if (removeElem.#right === null && removeElem.#left === null) {
-            if (removeElem.#value > removeElem.#parent.#value) {
-                removeElem.#parent.#right = null;
+        if (removeElem.#value < node.#value) {
+            if (node.#left.#value === removeElem.#value) {
+                // Удаление узла без потомков
+                if (removeElem.#right === null && removeElem.#left === null) {
+                    node.#left = null;
 
-                return true;
+                    return true;
+                }
+
+                // Удаление узла с двумя потомками
+                if (removeElem.#right && removeElem.#left) {
+                }
+
+                // Удаление узла с одним потомком
+                if (removeElem.#right === null || removeElem.#left === null) {
+                    node.#left = removeElem.#left;
+
+                    return true;
+                }
             }
 
-            removeElem.#parent.#left = null;
-
-            return true;
+            return this.remove(value, node.#left);
         }
 
-        // Удаление узла с двумя потомками
-        if (removeElem.#right && removeElem.#left) {
-            console.log('2');
-            return true;
-        }
+        if (removeElem.#value > node.#value) {
+            if (node.#right.#value === removeElem.#value) {
+                // Удаление узла без потомков
+                if (removeElem.#right === null && removeElem.#right === null) {
+                    node.#right = null;
 
-        // Удаление узла с одним потомком
-        if (removeElem.#right === null || removeElem.#left === null) {
-            const child = removeElem.#right ?? removeElem.#left;
+                    return true;
+                }
 
-            child.#parent = removeElem.#parent;
+                // Удаление узла с двумя потомками
+                if (removeElem.#right && removeElem.#left) {
+                    if (removeElem.#right.#left === null) {
+                        node.#right = removeElem.#left;
+                        node.#right.#right = removeElem.#right;
 
-            if (removeElem.#value < removeElem.#parent.#value) {
-                removeElem.#parent.#left = child;
+                        return true;
+                    } else {
+                        return this.remove(value, removeElem.#right.#left);
+                    }
+                }
 
-                return true;
+                // Удаление узла с одним потомком
+                if (removeElem.#right === null || removeElem.#left === null) {
+                    node.#right = removeElem.#right;
+
+                    return true;
+                }
             }
-
-            removeElem.#parent.#right = child;
-
-            return true;
+            return this.remove(value, node.#right);
         }
     }
 }
 
 const node = new Node();
-const arr = [10, 8, 14, 12, 16, 2, 1, 7, 27, 19, 28, 17, 29];
+const arr = [10, 8, 14, 12, 16, 26, 2, 1, 7, 19, 20, 28, 27, 17, 29];
 
 arr.forEach(item => node.insert(item));
 
-node.remove(28);
-// node.remove(1);
 console.log(node);
+// console.log(node.findByValue(0));
+node.remove(28);
