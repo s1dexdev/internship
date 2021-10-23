535547b690a2cabf955055284394bedd1f36f924
s1dexdev <maksym.se@gmail.com>
2021-10-23 13:04:57 +0300

    update

--- labs/block_3.js
+++ labs/block_3.js
@@ -147,41 +147,37 @@ const arr = [10, 8, 14, 12, 16, 15, 26, 2, 1, 7, 6, 19, 20, 28, 27, 17, 29];
 
 // Task 2 -----------
 
-function sortBubble(arr) {
-    const array = [...arr];
+Array.prototype.sortBubble = function () {
+    for (let i = 0; i < this.length; i++) {
+        for (let j = 0, k = 1; k < this.length; j++, k++) {
+            if (this[j] > this[k]) {
+                let tempValue = this[j];
 
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0, k = 1; k < array.length; j++, k++) {
-            let tempValue = null;
-
-            if (array[j] > array[k]) {
-                tempValue = array[j];
-                array[j] = array[k];
-                array[k] = tempValue;
+                this[j] = this[k];
+                this[k] = tempValue;
             }
         }
     }
 
-    return array;
-}
+    return this;
+};
 
-function sortSelection(arr) {
-    const array = [...arr];
+Array.prototype.sortSelection = function () {
+    let indexMaxValue = null;
+    let tempValue = null;
 
-    for (let i = array.length - 1; i >= 0; i--) {
-        let maxValue = array[0];
-        let tempValue = null;
+    for (let i = this.length - 1; i >= 0; i--) {
+        indexMaxValue = 0;
 
         for (let j = 1; j < i + 1; j++) {
-            if (array[j] > maxValue) {
-                maxValue = array[j];
+            if (this[j] > this[indexMaxValue]) {
+                indexMaxValue = j;
             }
         }
-
-        tempValue = array[i];
-        array[i] = maxValue;
-        array[array.indexOf(maxValue)] = tempValue;
+        tempValue = this[i];
+        this[i] = this[indexMaxValue];
+        this[indexMaxValue] = tempValue;
     }
 
-    return array;
-}
+    return this;
+};
