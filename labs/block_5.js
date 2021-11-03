8ace75dcfff391ec77235f60e5e156445d7e0ee4
s1dexdev <maksym.se@gmail.com>
2021-11-03 16:35:45 +0200

    update

--- labs/block_5.js
+++ labs/block_5.js
@@ -1,10 +1,10 @@
-const regexpPhoneNumber = /\+\d{2}\(\d{3}\)\d{3}-\d{2}-\d{2}/;
+const regexpPhoneNumber = /^\+\d{2}\(\d{3}\)\d{3}-\d{2}-\d{2}$/;
 
-const regexpEmail = /[a-zA-Z0-9]+(-?|\.?)[a-zA-Z0-9]+@[a-z]+\.[a-z]{2,10}/;
+const regexpEmail = /^[a-zA-Z0-9]+(-?|\.?)[a-zA-Z0-9]+@[a-z]+\.[a-z]{2,10}$/;
 
-const regexpSite = /https?:\/\/[a-zA-Z0-9]+-?[a-zA-Z0-9]+\.[a-z]{2,10}/;
+const regexpSite = /^https?:\/\/[a-zA-Z0-9]+-?[a-zA-Z0-9]+\.[a-z]{2,10}$/;
 
-const regexpPassword = /[\w]{6,25}/;
+const regexpPassword = /[\w\Q!#$%&*+,-./:;<=>?@~"'{}()[\]\E]{6,25}/;
 
 const regexpIpv4 =
     /((([0-2][0-5]{2}|[0-2][0-4]\d)|(\d{2})|(\d{1}))\.){3}(([0-2][0-5]{2}|[0-2][0-4]\d)|(\d{2})|(\d{1}))/;
