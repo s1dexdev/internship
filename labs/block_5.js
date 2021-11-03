f3e57e225f1ab7f2035974079c7202b21566f92f
s1dexdev <maksym.se@gmail.com>
2021-11-03 18:52:14 +0200

    update

--- labs/block_5.js
+++ labs/block_5.js
@@ -1,8 +1,9 @@
 const regexpPhoneNumber = /^\+\d{2}\(\d{3}\)\d{3}-\d{2}-\d{2}$/;
 
-const regexpEmail = /^[a-zA-Z0-9]+(-?|\.?)[a-zA-Z0-9]+@[a-z]+\.[a-z]{2,10}$/;
+const regexpEmail =
+    /^[a-zA-Z0-9]+(-?|\.?)[a-zA-Z0-9]+@([a-z]+\.?[a-z])+\.[a-z]{2,10}$/;
 
-const regexpSite = /^https?:\/\/[a-zA-Z0-9]+-?[a-zA-Z0-9]+\.[a-z]{2,10}$/;
+const regexpSite = /^https?:\/\/([a-zA-Z0-9]+-?[a-zA-Z0-9]+\.)+[a-z]{2,10}$/;
 
 const regexpPassword = /[\w\Q!#$%&*+,-./:;<=>?@~"'{}()[\]\E]{6,25}/;
 
