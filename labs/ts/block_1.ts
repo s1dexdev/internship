d464ff4f41c11fcf17c5231622271c3e5c386db8
s1dexdev <maksym.se@gmail.com>
2021-11-15 15:19:39 +0200

    added ts

--- labs/ts/block_1.ts
+++ labs/ts/block_1.ts
@@ -8,12 +8,12 @@ function isAnagram(strOne: string, strTwo: string): boolean {
         return false;
     }
 
-    for (let i = 0; i < wordOne.length; i++) {
+    for (let i: number = 0; i < wordOne.length; i++) {
         let counter1: number = 0;
         let counter2: number = 0;
-        let letterOne = wordOne[i];
+        let letterOne: string = wordOne[i];
 
-        for (let j = 0; j < wordOne.length; j++) {
+        for (let j: number = 0; j < wordOne.length; j++) {
             let letterTwo: string = wordOne[j];
 
             if (letterOne === letterTwo) {
@@ -37,7 +37,6 @@ function isAnagram(strOne: string, strTwo: string): boolean {
 
 // Task 2
 
-//  Преобразование числа в массив цифр
 function convertNumberToArray(number: number): number[] {
     const arr: number[] = [];
     let code: number = number;
@@ -52,22 +51,20 @@ function convertNumberToArray(number: number): number[] {
     return arr;
 }
 
-// Подсчет количества чисел
+
 interface IResult {
     [key: number]: number;
 }
 
-function calcQuantityDigits(number: number):IResult {
+function calcQuantityDigits(number: number): IResult {
     const numbers: number[] = convertNumberToArray(number);
     const result: IResult = {};
 
-    // Инициализация свойств объекта result
-    for (let i = 0; i < numbers.length; i++) {
+    for (let i: number = 0; i < numbers.length; i++) {
         result[numbers[i]] = 3;
     }
 
-    // Подсчет количества чисел
-    for (let i = 0; i < numbers.length; i++) {
+    for (let i: number = 0; i < numbers.length; i++) {
         result[numbers[i]]++;
     }
 
@@ -76,14 +73,14 @@ function calcQuantityDigits(number: number):IResult {
 
 // Task 4
 
-function calcQuantityUniqWords(str:string):number {
-    const arrayOfWords:string[] = str.toLowerCase().split(' ');
-    const uniqWords:string[] = [];
+function calcQuantityUniqWords(str: string): number {
+    const arrayOfWords: string[] = str.toLowerCase().split(' ');
+    const uniqWords: string[] = [];
 
-    for (let i = 0; i < arrayOfWords.length; i++) {
-        let count:number = 0;
+    for (let i: number = 0; i < arrayOfWords.length; i++) {
+        let count: number = 0;
 
-        for (let j = 0; j < arrayOfWords.length; j++) {
+        for (let j: number = 0; j < arrayOfWords.length; j++) {
             if (arrayOfWords[i] === arrayOfWords[j]) {
                 count++;
             }
@@ -100,20 +97,18 @@ function calcQuantityUniqWords(str:string):number {
 // Task 5
 
 interface IResult {
-    [key: string]:number
+    [key: string]: number;
 }
 
-function calcQuantityWords(str:string):IResult {
-    const arrayOfWords:string[] = str.toLowerCase().split(' ');
-    let result:IResult = {};
+function calcQuantityWords(str: string): IResult {
+    const arrayOfWords: string[] = str.toLowerCase().split(' ');
+    let result: IResult = {};
 
-    // Инициализация свойств объекта result
-    for (let i = 0; i < arrayOfWords.length; i++) {
+    for (let i: number = 0; i < arrayOfWords.length; i++) {
         result[arrayOfWords[i]] = 0;
     }
 
-    // Подсчет количества слов
-    for (let i = 0; i < arrayOfWords.length; i++) {
+    for (let i: number = 0; i < arrayOfWords.length; i++) {
         result[arrayOfWords[i]]++;
     }
 
@@ -122,15 +117,15 @@ function calcQuantityWords(str:string):IResult {
 
 // Task 6
 
-function fibonacci(number:number):number[] {
+function fibonacci(number: number): number[] {
     if (number === 0) {
         return [];
     }
 
-    const numbersFibonacci:number[] = [0, 1];
+    const numbersFibonacci: number[] = [0, 1];
 
-    for (let i = 0; i < number - 1; i++) {
-        let fib:number = numbersFibonacci[i] + numbersFibonacci[i + 1];
+    for (let i: number = 0; i < number - 1; i++) {
+        let fib: number = numbersFibonacci[i] + numbersFibonacci[i + 1];
 
         numbersFibonacci.push(fib);
     }
@@ -141,19 +136,19 @@ function fibonacci(number:number):number[] {
 // Task 7
 
 class Rectangle {
-    private width:number;
-    private height:number;
+    private width: number;
+    private height: number;
 
-    constructor(width:number, height:number) {
+    constructor(width: number, height: number) {
         this.width = width;
         this.height = height;
     }
 
-    get perimeter():number {
+    get perimeter(): number {
         return 2 * (this.width + this.height);
     }
 
-    get area():number {
+    get area(): number {
         return this.width * this.height;
     }
 }
@@ -161,22 +156,22 @@ class Rectangle {
 //Triangle
 
 class Triangle {
-    private sideA:number;
-    private sideB:number;
-    private sideC:number;
+    private sideA: number;
+    private sideB: number;
+    private sideC: number;
 
-    constructor(sideA:number, sideB:number, sideC:number) {
+    constructor(sideA: number, sideB: number, sideC: number) {
         this.sideA = sideA;
         this.sideB = sideB;
         this.sideC = sideC;
     }
 
-    get perimeter():number {
+    get perimeter(): number {
         return this.sideA + this.sideB + this.sideC;
     }
 
-    get area():number {
-        const semiPerimeter:number = this.perimeter / 2;
+    get area(): number {
+        const semiPerimeter: number = this.perimeter / 2;
 
         return Math.sqrt(
             semiPerimeter *
@@ -190,31 +185,30 @@ class Triangle {
 // Circle
 
 class Circle {
-    private radius:number;
+    private radius: number;
 
-    constructor(radius:number) {
+    constructor(radius: number) {
         this.radius = radius;
     }
 
-    get perimeter():number {
+    get perimeter(): number {
         return 2 * Math.PI * this.radius;
     }
 
-    get area():number {
+    get area(): number {
         return Math.PI * this.radius ** 2;
     }
 }
 
 // Task 8
-
-function factorial(number:number):number {
-    let factorial:number = 1;
+function factorial(number: number): number {
+    let factorial: number = 1;
 
     if (number <= 1) {
         return factorial;
     }
 
-    for (let i = 0; i < number; i++) {
+    for (let i: number = 0; i < number; i++) {
         factorial *= number - i;
     }
 
@@ -222,3 +216,241 @@ function factorial(number:number):number {
 }
 
 // Task 9
+type cb = (elem: number, flag?: boolean) => boolean;
+
+function calcSumElems(arr: number[], callback: cb): number {
+    let sum: number = 0;
+
+    for (let i: number = 0; i < arr.length; i++) {
+        if (callback(arr[i])) {
+            sum += arr[i];
+        }
+    }
+
+    return sum;
+}
+
+// Task 10
+
+function getQuantityDigits(
+    arr: number[],
+    callback: cb,
+    isFlag: boolean,
+): number {
+    isFlag = isFlag || false;
+
+    let quantity: number = 0;
+
+    for (let i: number = 0; i < arr.length; i++) {
+        let isPrime: boolean = true;
+
+        if (isFlag) {
+            for (let j: number = 2; j < arr[i]; j++) {
+                if (arr[i] % j === 0) {
+                    isPrime = false;
+
+                    break;
+                }
+            }
+        }
+
+        if (callback(arr[i], isPrime)) {
+            quantity++;
+        }
+    }
+
+    return quantity;
+}
+
+// Task 11
+
+function convertDecimalToBinary(value: number): number {
+    let number: number = value;
+    let result: number[] = [];
+
+    while (number >= 1) {
+        result.push(number % 2);
+        number = Math.floor(number / 2);
+    }
+
+    return Number(result.reverse().join(''));
+}
+
+function convertBinaryToDecimal(value: number): number {
+    const numbers: number[] = convertNumberToArray(value);
+    let result: number = 0;
+
+    for (let i: number = 0; i < numbers.length; i++) {
+        result += numbers[i] * 2 ** (numbers.length - 1 - i);
+    }
+
+    return result;
+}
+
+// Task 12
+
+function calcSumDigitsArr(arr: number[][], callback:cb): number {
+    let sum: number = 0;
+
+    for (let i:number = 0; i < arr.length; i++) {
+        for (let j: number = 0; j < arr[i].length; j++) {
+            let number: number = arr[i][j];
+
+            if (callback(number)) {
+                sum += number;
+            }
+        }
+    }
+
+    return sum;
+}
+
+function calcQuantityDigitsArr(arr: number[][], callback:cb, isFlag:boolean): number {
+    isFlag = isFlag || false;
+    let quantity: number = 0;
+
+    for (let i: number = 0; i < arr.length; i++) {
+        for (let j: number = 0; j < arr[i].length; j++) {
+            let isPrime: boolean = true;
+            let number: number = arr[i][j];
+
+            if (isFlag) {
+                for (let k: number = 2; k < number; k++) {
+                    if (number % k === 0) {
+                        isPrime = false;
+
+                        break;
+                    }
+                }
+            }
+
+            if (callback(number, isPrime)) {
+                quantity++;
+            }
+        }
+    }
+
+    return quantity;
+}
+
+// Task 13
+
+function getSumElems(min: number, max: number, callback:cb): number {
+    let sum: number = 0;
+
+    for (let i: number = min; i <= max; i++) {
+        if (callback(i)) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
+
+// Task 14
+
+function calcMeanValue(arr: number[][]): number {
+    const arrayOfNumbers: number[] = arr.flat();
+    let result: number = 0;
+    let sum: number = 0;
+
+    for (let i: number = 0; i < arrayOfNumbers.length; i++) {
+        sum += arrayOfNumbers[i];
+    }
+
+    result = Math.floor(sum / arrayOfNumbers.length);
+
+    return result;
+}
+
+// Task 15
+
+function matrixTranspotion(matrix: number[][]): number[][] {
+    const matrixT: number[][] = [];
+
+    for (let i: number = 0; i < matrix.length; i++) {
+        matrixT.push([]);
+    }
+
+    for (let i: number = 0; i < matrix.length; i++) {
+        for (let j: number = 0; j < matrix[i].length; j++) {
+            matrixT[j].push(matrix[i][j]);
+        }
+    }
+
+    return matrixT;
+}
+
+// Task 16
+
+function matrixAddition(matrix1: number[][], matrix2: number[][]): number[][] {
+    const result: number[][] = [];
+
+    for (let i: number = 0; i < matrix1.length; i++) {
+        result.push([]);
+    }
+
+    for (let i: number = 0; i < matrix1.length; i++) {
+        for (let j: number = 0; j < matrix1[i].length; j++) {
+            const number: number = matrix1[i][j] + matrix2[i][j];
+
+            result[i].push(number);
+        }
+    }
+
+    return result;
+}
+
+// Task 17
+
+function deleteRowWithValue(array: number[][], value:number) {
+    value = value || 0;
+    let result: number[][] = array;
+    let indexes: number[] = [];
+
+    for (let i: number = 0; i < array.length; i++) {
+        if (array[i].includes(value)) {
+            const index: number = i;
+
+            if (!indexes.includes(index)) {
+                indexes.push(index);
+            }
+        }
+    }
+
+    indexes.reverse(); 
+
+    for (let j: number = 0; j < indexes.length; j++) {
+        result.splice(indexes[j], 1);
+    }
+
+    return result;
+}
+
+function deleteColumnWithValue(array: number[][], value: number) {
+    value = value || 0;
+    const result: number[][] = array;
+    let indexes: number[] = [];
+
+    for (let i: number = 0; i < array.length; i++) {
+        for (let j: number = 0; j < array[i].length; j++) {
+            if (array[j][i] === value) {
+                const index = i;
+
+                if (!indexes.includes(index)) {
+                    indexes.push(index);
+                }
+            }
+        }
+    }
+
+    indexes.reverse(); 
+
+    for (let i: number = 0; i < indexes.length; i++) {
+        for (let j: number = 0; j < array.length; j++) {
+            result[j].splice(indexes[i], 1);
+        }
+    }
+
+    return result;
+}
