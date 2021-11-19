45fb530ac77dae21346801eadefe042411ad1137
s1dexdev <maksym.se@gmail.com>
2021-11-19 12:39:00 +0200

    added block_2

--- labs/ts/block_2.ts
+++ labs/ts/block_2.ts
@@ -1,83 +1,288 @@
+// Task 1
+
+// Call
+interface ICall {
+    [key: symbol]: Function;
+}
+
 interface Function {
     customCall<T, U>(context: T, ...args: U[]): T;
 }
 
-Function.prototype.customCall = function (context, ...args) {
-    const func = this;
+Function.prototype.customCall = function <T, U>(context: T, ...args: U[]): T {
+    let symbol: symbol = Symbol();
 
-    (function (): void {
-        context = context || this;
+    if (typeof context !== 'object') {
+        const result: ICall = Object(context);
 
-        if (typeof context === 'object') {
-            let method: symbol = Symbol();
+        result[symbol] = this;
+        result[symbol](...args);
 
-            context[method] = func;
-            context[method](...args);
+        return context;
+    }
 
-            delete context[method];
-        } else {
-            const result = Object(context);
+    const result: ICall = Object.assign({}, context, { [symbol]: this });
 
-            result.method = func;
-            result.method(...args);
-        }
-    })();
+    result[symbol](...args);
 
     return context;
 };
 
-const obj = {
-    x: 10,
-};
+// Bind
 
-function foo() {
-    console.log(this);
+interface IBindResult {
+    [key: symbol]: Function;
 }
 
-foo.customCall(obj);
+type bindedFunc = <A>(...args: A[]) => IBindResult;
 
-// interface K {
-//     [Symbol: symbol]: Function;
-// }
+interface Function {
+    customBind<T, U>(context: T, ...args: U[]): bindedFunc;
+}
 
-// interface Function {
-//     customCall<T, U>(context: T & K, ...args: U[]): T;
-// }
+Function.prototype.customBind = function <T, U>(
+    context: T,
+    ...args: U[]
+): bindedFunc {
+    const func: Function = this;
 
-// Function.prototype.customCall = function <T, U>(
-//     context: T & K,
-//     ...args: U[]
-// ): T {
-//     let symbol: symbol = Symbol();
+    return function <A>(...args2: A[]): IBindResult {
+        let symbol: symbol = Symbol();
 
-//     if (typeof context === 'object') {
-//         context[symbol] = this;
-//         context[symbol](...args);
+        if (typeof context !== 'object') {
+            const result: IBindResult = Object(context);
 
-//         delete context[symbol];
-//     } else {
-//         const result = Object(context);
+            result[symbol] = func;
+            result[symbol](...args);
 
-//         result[symbol] = this;
-//         result[symbol](...args);
-//     }
+            return result;
+        }
 
-//     return context;
-// };
+        const result: IBindResult = Object.assign({}, context, {
+            [symbol]: func,
+        });
 
-// type K = {
-//     [Symbol: symbol]: Function;
-// };
+        result[symbol](...args, ...args2);
 
-// interface Function {
-//     customCall<T, U>(context: T & K, ...args: U[]): T;
-// }
+        return result;
+    };
+};
 
-// Function.prototype.customCall = function <T, U>(context: T, ...args: U[]): T {
-//     let symbol: symbol = Symbol();
+// Task 2
 
-//     let c: T = Object.assign({}, context, { symbol });
-//     // let c2: T = c as T;
+interface IThisValue {
+    [key: symbol]: Function;
+}
 
-//     return c;
-// };
+// Map
+
+interface Array<T> {
+    customMap(callback: Function, thisValue?: IThisValue): T[];
+}
+
+Array.prototype.customMap = function <T>(
+    callback: Function,
+    thisValue?: IThisValue,
+): T[] {
+    thisValue = thisValue || undefined;
+
+    const result: any[] = [];
+    let symbol: symbol = Symbol();
+
+    if (thisValue) {
+        thisValue[symbol] = callback;
+    }
+
+    for (let i: number = 0; i < this.length; i++) {
+        const currentItem: any = this[i];
+        const index: number = i;
+
+        if (thisValue) {
+            result.push(thisValue[symbol](currentItem, index, this));
+        } else {
+            result.push(callback(currentItem, index, this));
+        }
+    }
+
+    if (thisValue) {
+        delete thisValue[symbol];
+    }
+
+    return result;
+};
+
+// Filter
+
+interface Array<T> {
+    customFilter(callback: Function, thisValue?: IThisValue): T[];
+}
+
+Array.prototype.customFilter = function <T>(
+    callback: Function,
+    thisValue?: IThisValue,
+): T[] {
+    thisValue = thisValue || undefined;
+
+    const result: any[] = [];
+    let symbol: symbol = Symbol();
+
+    if (thisValue) {
+        thisValue[symbol] = callback;
+    }
+
+    for (let i: number = 0; i < this.length; i++) {
+        const currentItem: any = this[i];
+        const index: number = i;
+
+        if (thisValue) {
+            const isValid: boolean = thisValue[symbol](
+                currentItem,
+                index,
+                this,
+            );
+
+            if (isValid) {
+                result.push(currentItem);
+            }
+        } else {
+            const isValid: boolean = callback(currentItem, index, this);
+
+            if (isValid) {
+                result.push(currentItem);
+            }
+        }
+    }
+
+    if (thisValue) {
+        delete thisValue[symbol];
+    }
+
+    return result;
+};
+
+// forEach
+interface Array<T> {
+    customForEach(callback: Function, thisValue?: IThisValue): void;
+}
+
+Array.prototype.customForEach = function (
+    callback: Function,
+    thisValue?: IThisValue,
+): void {
+    thisValue = thisValue || undefined;
+    const array: any[] = [...this];
+    const symbol: symbol = Symbol();
+
+    if (thisValue) {
+        thisValue[symbol] = callback;
+    }
+
+    for (let i: number = 0; i < array.length; i++) {
+        const currentItem: any = array[i];
+        const index: number = i;
+
+        if (thisValue) {
+            thisValue[symbol](currentItem, index, array);
+        } else {
+            callback(currentItem, index, array);
+        }
+    }
+
+    if (thisValue) {
+        delete thisValue[symbol];
+    }
+};
+
+// Reduce
+
+interface Array<T> {
+    customReduce<U>(callback: Function, initialValue?: U): U;
+}
+
+Array.prototype.customReduce = function <U>(
+    callback: Function,
+    initialValue?: U,
+): U {
+    if (this.length === 0 && initialValue === undefined) {
+        throw new Error('Reduce of empty this with no initial value');
+    }
+
+    let accumulator: any = initialValue ?? this[0];
+
+    if (
+        (this.length === 1 && initialValue === undefined) ||
+        (initialValue && this.length === 0)
+    ) {
+        return accumulator;
+    }
+
+    let currentValue: any = accumulator === initialValue ? this[0] : this[1];
+
+    for (let i: number = 0; i < this.length; i++) {
+        const index: number = i;
+
+        accumulator = callback(accumulator, currentValue, index, this);
+        currentValue = this[i + 1];
+    }
+
+    return accumulator;
+};
+
+// Task 3
+interface IObjOne {
+    oldValue: number;
+    newValue: number;
+    numberIter: number;
+
+    [Symbol.iterator](): Generator;
+}
+
+const obj: IObjOne = {
+    oldValue: 0,
+    newValue: 1,
+    numberIter: 10,
+
+    *[Symbol.iterator]() {
+        for (let i = 0; i < this.numberIter; i++) {
+            yield this.oldValue;
+
+            this.newValue = this.oldValue + this.newValue;
+            this.oldValue = this.newValue - this.oldValue;
+        }
+    },
+};
+
+// Solution 2
+
+interface IObjTwo {
+    oldValue: number;
+    newValue: number;
+    numberIter: number;
+
+    [Symbol.iterator](): this;
+
+    next(): IteratorResult<number>;
+}
+
+const obj2: IObjTwo = {
+    oldValue: 0,
+    newValue: 1,
+    numberIter: 10,
+
+    [Symbol.iterator]() {
+        return this;
+    },
+
+    next() {
+        this.numberIter--;
+
+        if (this.numberIter >= 0) {
+            this.newValue = this.oldValue + this.newValue;
+            this.oldValue = this.newValue - this.oldValue;
+
+            return { value: this.oldValue, done: false };
+        }
+
+        return { value: undefined, done: true };
+    },
+};
